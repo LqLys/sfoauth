6f22c2bdd9a127e89f46863b261e3b4a69218057
lqlys <lys.lukasz555@gmail.com>
2022-03-23 10:43:05 +0100

    oauth2 check

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {OAuthService} from "angular-oauth2-oidc";
 import {Router} from "@angular/router";
 import {authCodeFlowConfig} from "../../../auth-code-flow.config";
+import {HttpClient, HttpHeaders } from "@angular/common/http";
 
 @Component({
   selector: 'app-login',
@@ -10,7 +11,7 @@ import {authCodeFlowConfig} from "../../../auth-code-flow.config";
 })
 export class LoginComponent implements OnInit {
   claims: any;
-  constructor(private oauthService: OAuthService, private router: Router) { }
+  constructor(private oauthService: OAuthService, private router: Router, private http: HttpClient) { }
 
   ngOnInit(): void {
     this.configSso();
@@ -29,7 +30,10 @@ export class LoginComponent implements OnInit {
 
   get token(){
     this.claims = this.oauthService.getIdentityClaims();
-
+    console.log('======')
+    // console.log(this.oauthService.getAccessToken());
+    console.log(this.oauthService.getIdToken());
+    console.log('======')
     console.log('CLAIMS', this.claims)
     // debugger
     if(this.claims){
@@ -39,4 +43,15 @@ export class LoginComponent implements OnInit {
     return this.claims ? this.claims : null;
   }
 
+  test2() {
+    const headers = new HttpHeaders()
+      .set("Authorization", "Bearer " + this.oauthService.getIdToken());
+    this.http.get('http://localhost:8080/test2', {headers})
+      .subscribe(a => console.log(a));
+  }
+
+  test1() {
+    this.http.get('http://localhost:8080/test1  ')
+      .subscribe(a => console.log(a));
+  }
 }
